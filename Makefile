2965ecb99ad33d096f2993da35755af349e10811
zvv <zvv1287@yandex.ru>
2023-02-04 14:00:10 +0300

    Добавлен flake8 и исправлены стилистические ошибки

---
+++ Makefile
@@ -0,0 +1,14 @@
+
+build:
+	poetry build
+
+publish:
+	poetry publish --dry-run
+
+package-install:
+	python3 -m pip install --user --force-reinstall dist/*.whl
+
+lint:
+	poetry run flake8 gendiff/
+
+
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,23 +1,21 @@
 import argparse
 
+parser = argparse.ArgumentParser(
+    prog='gendiff',
+    description='Compares two configuration files and shows a difference.',
+)
 
-parser  = argparse.ArgumentParser(
-                    prog = 'gendiff',
-                    description = 'Compares two configuration files and shows a difference.',
-                 )
+parser.add_argument('first_file')
+parser.add_argument('second_file')
 
-
-parser.add_argument('first_file')    
-parser.add_argument('second_file')  
-
-parser.add_argument('-f', '--format', help='set format of output') 
+parser.add_argument('-f', '--format', help='set format of output')
 
 args = parser.parse_args()
 
 
+def main():
+    print('Вычислитель отличий')
 
 
-
-
-
-
+if __name__ == '__main__':
+    main()
